38b2d54424531829548feb95d692c6a5ee8efe0e
Slavavw <slavavw1978@gmail.com>
2022-05-22 14:56:34 +0300

    fixed: corrcted js in 05-merge-styles (compressed code and use promise)

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,40 +1,33 @@
 const fs = require('fs');
 const path = require('path');
-const {promise} = fs;
+const promise = fs.promises;
 const outputDir = path.join(__dirname,'project-dist','bundle.css');
 const inputDir = path.join(__dirname,'styles');
 
-
-let resFile = (function(){
-    try{
-        let exit = fs.existsSync(outputDir);
-        if (!exit) return fs.createWriteStream(outputDir,'utf-8');        
-        else {
-            fs.unlink(outputDir, (err) => { 
-                if (err) console.log(err)    
-                else console.log( 'bundle.css был удален')
-              })
-              return fs.createWriteStream(outputDir,'utf-8');
-        }
+(async function(){      
+    let exit = fs.existsSync(outputDir);
+    if (!exit) {
+        await promise.mkdir(path.join(__dirname,'project-dist'));
+        return fs.createWriteStream(outputDir,'utf-8');
     }
-    catch(e) {console.log(e);}
-})();
+    else{
+        let response =  await promise.unlink(outputDir);
+        console.log( 'bundle.css был удален');            
+    }        
+    return fs.createWriteStream(outputDir,'utf-8');       
+})().then( Execute).catch(err=> console.log(err));
 
 
-
-fs.readdir(inputDir,{encoding:"utf-8"}, (error,files)=>{
-    if (error) console.log(error);
-    console.log(files);        
-    for ( let file of files){        
+async function Execute(resFile){    
+    let files = await promise.readdir(inputDir,{encoding:"utf-8"});
+    files.forEach( file =>{    
         if ( /(css)$/i.test(path.extname(file))){
             const readableStream = fs.createReadStream(path.join(inputDir,file),'utf-8');
             readableStream.on("data",chunk =>{                
-                resFile.write(chunk.toString(),error=>{console.log(error)});
-                //fs.appendFile(outputDir,chunk,()=>{console.log(chunk.toString())});
+                resFile.write(chunk.toString(),error=>{console.log(error)});                
             });
             readableStream.on("end",()=>{console.log(`файл ${file} записан`);});        
         }
-    }
-})
+    })
+}
 
-    
