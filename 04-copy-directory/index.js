097e66018770a123594faaae2128d6a3b7a7611d
Slavavw <slavavw1978@gmail.com>
2022-06-07 11:15:26 +0300

    fix: remobe exitSync add exits change code- worked corrext

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,24 +6,28 @@ const fs_promise = fs.promises;
 const outputDir = path.join(__dirname,'files');
 const inputDir = path.join(__dirname,'files-copy');
 
-(async function(){
+async function start(){
     try {
-        const exists = fs.existsSync(inputDir);
-        if (!exists)  await fs_promise.mkdir(inputDir);
-        else {console.log('Папка была ранее создана обновим ее, перезаписав');
-            let files =  await fs_promise.readdir(inputDir);            
-            for ( let file of files ){            
-                console.log(file);
-                await fs_promise.unlink(path.join(inputDir,file));                
-            }             
-        }
-    } catch (e) { console.log(e); }
-})();  
+        await fs.exists(inputDir, async (exit) =>{            
+            if (!exit) {await fs_promise.mkdir(inputDir); await readFileInDir();}
+            else {
+                console.log('Папка была ранее создана обновим ее, перезаписав');
+                let files =  await fs_promise.readdir(inputDir);            
+                for await ( let file of files ){ 
+                    console.log("Удаляем файл:",file);
+                    await fs_promise.unlink(path.join(inputDir,file));                
+                }
+                await readFileInDir();             
+            }            
+        });                
+    }catch (e) { console.log(e); }
+};  
 
 async function readFileInDir(){
     try {
         const files = await fs_promise.readdir(outputDir);
-        for (const file of files){
+        for await (const file of files){
+            console.log("создаем файл:",file);
             let output = fs.createReadStream(path.join(outputDir,file),"utf8");
             let input = fs.createWriteStream(path.join(__dirname,'files-copy',file),"utf-8");
             output.pipe(input);       
@@ -31,4 +35,4 @@ async function readFileInDir(){
      }catch (err) { console.error(err);}
 }
 
- readFileInDir();
+ start();
